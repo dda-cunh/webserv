b4b2db8ac3e295452951f89b8a371314d4614fc9
dda-cunh <dda-cunh@student.42lisboa.com>
2024-06-12 18:59:49 +0100

    Some http logic

---
+++ includes/http.hpp
@@ -0,0 +1,32 @@
+#ifndef HTTP_HPP
+# define HTTP_HPP
+
+/*******************************  METHODS  ********************************/
+typedef enum	e_method
+{
+	HTTP_M_UNHANDLED,
+	HTTP_M_DELETE,
+	HTTP_M_POST,
+	HTTP_M_GET,
+	__LEN_HTTP_METHODS
+}	HTTP_METHODS;
+/**************************************************************************/
+
+/****************************  RESPONSE CODES  ****************************/
+typedef enum	e_http_status_cides
+{
+	HTTP_SC_OK = 200,
+	HTTP_SC_CREATED = 201,
+	HTTP_SC_NO_CONTENT = 204,	//The most common success code for DELETE
+	HTTP_SC_BAD_REQUEST = 400,
+	HTTP_SC_UNAUTHORIZED = 401,	//Connecting to server
+	HTTP_SC_FORBIDDEN = 403,	//Getting a resource
+	HTTP_SC_NOT_FOUND = 404,
+	HTTP_SC_CONFLICT = 409,		//If the data to post already exist and shouldnt be overwritten
+	HTTP_SC_INTERNAL_SERVER_ERROR = 500,
+	HTTP_SC_BAD_GATEWAY = 502,
+	HTTP_SC_SERVICE_UNAVAILABLE = 503
+}	HTTP_STATUS_CODE;
+/**************************************************************************/
+
+#endif
