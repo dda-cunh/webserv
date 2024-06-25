983890bcd46cb1f52a6d3dac09e8e1e4d9ab6d6b
dda-cunh <dda-cunh@student.42lisboa.com>
2024-06-25 21:07:53 +0100

    Initial implementation of Request Class

--- includes/classes/Request.hpp
+++ includes/classes/Request.hpp
@@ -4,6 +4,12 @@
 
 #define NO_SUCH_HEADER	""
 
+typedef enum	e_response_flag
+{
+	NO_FLAG,
+	_400
+}	ResponseFlag;
+
 /**
  *	@brief Represents an HTTP request received by the server.
  *	Depends on a ServerConfig instance and client_fd.
@@ -16,36 +22,44 @@ class Request
 
 		Request(ServerConfig const&, Socket const&);
 
-		ServerConfig const&	getServerConfig()	const;
-		HTTP_VERSION const&	getVersion()		const;
-		std::ustring const&	getBody()			const;
-		std::string const&	getUri()			const;
-		HTTP_METHOD const&	getMethod()			const;
-		Socket const&		getClientFD()		const;
+		ServerConfig const&			getServerConfig()	const;
+		HTTP_VERSION const&			getVersion()		const;
+		std::ustring const&			getBody()			const;
+		ResponseFlag const&			getFlag()			const;
+		std::string const&			getUri()			const;
+		HTTP_METHOD const&			getMethod()			const;
+		Socket const&				getClientFD()		const;
 
-		std::string const	getHeader(std::string);
+		std::string const			getHeader(std::string const&);
 
 	private:
-		ServerConfig const	_server_config;
-		Socket const		_client_fd;
-		long const			_unique_id;
+		ServerConfig const			_server_config;
+		Socket const				_client_fd;
 
-		HTTP_VERSION		_version;
-		std::ustring		_body;
-		std::string			_uri;
-		HTTP_METHOD			_method;
-		StrStrMap			_headers;
+		HTTP_VERSION				_version;
+		std::ustring				_body;
+		ResponseFlag				_flag;
+		std::string					_uri;
+		HTTP_METHOD					_method;
+		StrStrMap					_headers;
 
 		Request();
 		Request(Request const & src);
 		Request & operator=(Request const & rhs);
 
-		std::ustring		getNextChunkClient();
-		void				readClient();
-		void				parseBody(std::ustring const&);
+		std::ustring				getNextChunkClient();
+		void						putHeader(std::string const&,
+										std::string const&);
+		void						parseHeaderLine(std::stringstream &);
+		void						parseBody(std::ustring const&);
+		void						readClient();
 
-		static char* const	_expected_version;
+		static unsigned int const	_max_request_size;
+		static std::string const	_expected_version;
 
-		static long			genUniqueID(Socket const&);
-		static void			lowerStr(std::string &);
+		static HTTP_VERSION			identifyHTTPVersion(std::string const&);
+		static std::string const	seekCRLF(std::ustring const&,
+										std::ustring::size_type &);
+		static std::string			lowerStr(std::string const&);
+		static HTTP_METHOD			identifyMethod(std::string const&);
 };
