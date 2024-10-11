4e717ea4db36641e63f91652f493c57453e060fa
MrFacundo <ftroitero@gmail.com>
2024-10-11 21:03:08 +0100

    clean diff

--- includes/classes/Response.hpp
+++ includes/classes/Response.hpp
@@ -10,38 +10,38 @@
 #include <algorithm>
 
 class Response {
-public:
-    ~Response();
-    std::string const &getResponse() const;
-    Response(Request const &, ServerBlocks const &);
-    
-    void setBody(const std::string &body);
-    void setHeader(const std::string &key, const std::string &value);
+	public:
+		Response(Request const &, ServerBlocks const &);
+		~Response();
+		std::string				const &getResponse() const;
+		
+		void					setBody(const std::string &body);
+		void					setHeader(const std::string &key, const std::string &value);
 
-private:
-    Http::STATUS_CODE _statusCode;
-    StrStrMap _headers;
-    std::string _body;
-    std::string _response;
-    Request _request;
-    ServerBlocks const &_serverBlocks;
-    ServerLocation *_matchedLocation;
-    std::string _redirectionPath;
+	private:
+		Http::STATUS_CODE		_statusCode;
+		StrStrMap				_headers;
+		std::string				_body;
+		std::string				_response;
+		Request					_request;
+		ServerBlocks const &	_serverBlocks;
+		ServerLocation *		_matchedLocation;
+		std::string				_redirectionPath;
 
-    void dispatchMethod();
-    void handleGETMethod();
-    void handlePOSTMethod();
-    void handleDELETEMethod();
-    void handleMethodNotAllowed();
-    void handleCGI();
+		void					dispatchMethod();
+		void 					handleGETMethod();
+		void					handlePOSTMethod();
+		void					handleDELETEMethod();
+		void					handleMethodNotAllowed();
+		void					handleCGI();
 
-    void setMatchedLocation();
-    void setCommonHeaders();
-    void setResponse();
-    void setStatusAndReadResource(Http::STATUS_CODE statusCode, std::string uri = "");
+		void					setMatchedLocation();
+		void					setCommonHeaders();
+		void					setResponse();
+		void					setStatusAndReadResource(Http::STATUS_CODE statusCode, std::string uri = "");
 
-    void readResource(const std::string &uri, bool isErrorResponse = false);
-    std::string getHeadersStr();
-    bool isRedirection();
-    void handleRedirection();
-};
+		void					readResource(const std::string &uri, bool isErrorResponse = false);
+		std::string				getHeadersStr();
+		bool					isRedirection();
+		void					handleRedirection();
+	};
