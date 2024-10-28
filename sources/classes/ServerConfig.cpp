c508875d0a47f5559305612a347b3736eb9a1c1f
MrFacundo <ftroitero@gmail.com>
2024-10-28 19:53:35 +0000

    gco ServerConfig bug & git conflict cleaning

--- sources/classes/ServerConfig.cpp
+++ sources/classes/ServerConfig.cpp
@@ -65,7 +65,7 @@ ServerConfig &ServerConfig::operator=(const ServerConfig &serverConfig)
 	this->_host = serverConfig.getHost();
 	this->_port = serverConfig.getPort();
 
-	vectorSize = serverConfig.getLocationBlocksSize();
+	vectorSize = serverConfig.getServerNamesSize();
 	for (size_t i = 0; i < vectorSize; i++)
 		this->_serverNames.push_back(serverConfig.getServerName(i) );
 
